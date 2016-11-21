2de62e4fcb02cfc990e0df0880faee8786d67f04
yakovalenko <jaroslawkovalenko@jmail.com>
2016-11-21 14:04:57 +0200

    recreate

---
+++ src/initDB.sql
@@ -0,0 +1,46 @@
+create table DEVELOPERS(
+developer_id integer PRIMARY KEY NOT NULL,
+developer_name character varying NOT NULL,
+developer_age smallint NOT NULL,
+developer_adress character varying,
+developer_join_date date
+);
+
+alter table DEVELOPERS add column salary numeric;
+
+create table PROJECTS(
+ project_id integer PRIMARY KEY NOT NULL,
+ project_name character varying NOT NULL,
+ project_developer_id integer references DEVELOPERS(developer_id) NOT NULL,
+ project_start_timestamp date NOT NULL
+);
+
+create table COMPANIES(
+companie_id integer PRIMARY KEY NOT NULL,
+companie_name character varying NOT NULL,
+companie_project_id integer references PROJECTS(project_id) NOT NULL
+);
+
+create table CUSTOMERS(
+customer_id integer PRIMARY KEY NOT NULL,
+customer_name character varying NOT NULL,
+customer_companie_id integer references COMPANIES(companie_id) NOT NULL
+);
+CREATE TABLE DEVELOPERS_SKILLS
+(
+  dev_id integer  NOT NULL,
+  skill_id integer NOT NULL
+);
+
+Alter table DEVELOPERS_SKILLS add constraint dev_id_fk foreign key(dev_id) references DEVELOPERS(developer_id );
+Alter table DEVELOPERS_SKILLS add constraint skill_id_fk foreign key(skill_id) references SKILLS(skill_id);
+
+CREATE TABLE DEVELOPERS_Projects
+(
+  dev_id integer NOT NULL,
+  project_id integer NOT NULL
+);
+
+Alter table DEVELOPERS_Projects add constraint dev_id_fk foreign key(dev_id) references DEVELOPERS(developer_id );
+Alter table DEVELOPERS_Projects add constraint project_id_fk foreign key(project_id) references PROJECTS(project_id);
+
