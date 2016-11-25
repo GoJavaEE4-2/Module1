5685133c8d6cfb0c14b68d268e1ab12e4f974bf2
yakovalenko <jaroslawkovalenko@jmail.com>
2016-11-25 17:50:02 +0200

    moved the developer table under  PROJECTS & COMPANIES tables

--- src/5.sql
+++ src/5.sql
@@ -1,1 +1,2 @@
-select customers.customer_name, projects.project_cost from PROJECTS join (select min(project_cost) from PROJECTS group by project_company_id);
+select customer_name from customers as c INNER JOIN projects as p on c.customer_id = p.project_customer_id
+WHERE project_cost = (SELECT min(project_cost) FROM projects);
