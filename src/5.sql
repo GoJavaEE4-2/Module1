0db16e88d1020acd603c4554ddbdc507a11d0d11
yakovalenko <jaroslawkovalenko@jmail.com>
2016-11-25 17:58:48 +0200

    query 5, find most unusefull company

--- src/5.sql
+++ src/5.sql
@@ -1,2 +1,2 @@
-select customer_name from customers as c INNER JOIN projects as p on c.customer_id = p.project_customer_id
+select customer_name from customers as c LEFT JOIN projects as p on c.customer_id = p.project_customer_id
 WHERE project_cost = (SELECT min(project_cost) FROM projects);
