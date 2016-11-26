eb65b6ebea41134c5cfc718bbc6af3309cacd20e
VladRybkin <Footballer0@mail.ru>
2016-11-26 20:51:56 +0200

    references companys update

--- src/2.sql
+++ src/2.sql
@@ -1,4 +1,4 @@
 select * from projects as d inner join developers as  dp on d.id = dp.dev_id
-where dp.project_id = (select id from projects where cost = (select max(cost) from projects) )
+where dp.project_id = (select id from projects where cost = (select max(cost) from developers) )
 
 -- Найти самый дорогой проект (исходя из ЗП разработчиков).
