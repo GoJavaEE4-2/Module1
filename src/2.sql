86fc3efbe0dfddfc75310c4a6c995cb060d351c4
VladRybkin <Footballer0@mail.ru>
2016-11-26 20:40:50 +0200

    references companys update

---
+++ src/2.sql
@@ -0,0 +1,4 @@
+select * from projects as d inner join developers as  dp on d.id = dp.dev_id
+where dp.project_id = (select id from projects where cost = (select max(cost) from projects) )
+
+-- Найти самый дорогой проект (исходя из ЗП разработчиков).
