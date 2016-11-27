01eff75610c18c648d61179812377e864a6f414b
VladRybkin <Footballer0@mail.ru>
2016-11-27 13:58:07 +0200

    references companys update

--- src/5.sql
+++ src/5.sql
@@ -1,2 +1,3 @@
 select customer_name from customers as c LEFT JOIN projects as p on c.customer_id = p.project_customer_id
 WHERE project_cost = (SELECT min(project_cost) FROM projects);
+-- Найти клиента (customer), которая приносит меньше всего прибыли компании (company) для каждой из компаний .
