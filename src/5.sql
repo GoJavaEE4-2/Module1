6cb355f71bc161d4e42271615a12cfccd342cb92
yakovalenko <jaroslawkovalenko@jmail.com>
2016-12-01 18:46:22 +0200

    5.sql fixed

--- src/5.sql
+++ src/5.sql
@@ -1,5 +1,6 @@
-SELECT customers.customer_name, min(projects.project_cost) AS min FROM customers
-JOIN projects
-ON customers.customer_id = projects.project_customer_id
-GROUP BY customers.customer_name
-ORDER BY min LIMIT 1;
+SELECT companie_name,  c.min_cost, project_name, customer_name
+FROM (SELECT companies.companie_name, min(projects.project_cost) AS min_cost, companies.companie_id FROM companies
+  JOIN projects ON companies.companie_id = projects.project_company_id
+GROUP BY companie_name, companie_id)
+  AS c JOIN projects p ON c.companie_id = p.project_company_id AND c.min_cost = project_cost
+JOIN customers ON p.project_customer_id = customers.customer_id;
