125835734e0c1941cc684ec7f92cefa5ec49a839
VladRybkin <Footballer0@mail.ru>
2016-12-03 19:45:39 +0200

    Merge branch 'master' of https://github.com/GoJavaEE4-2/Module1
    # Conflicts:
    #	src/initDB.sql

--- src/initDB.sql
+++ src/initDB.sql
@@ -2,34 +2,39 @@ DROP SCHEMA IF EXISTS module_1;
 
 CREATE SCHEMA IF NOT EXISTS module_1;
 
-alter table if exists public.developers_skills drop constraint developers_skills_developer_id_fkey;
-alter table if exists public.developers_skills drop constraint developers_skills_skill_id_fkey;
-alter table if exists public.projects drop constraint projects_project_customer_id_fkey;
-alter table if exists public.projects drop constraint projects_project_company_id_fkey;
-alter table if exists public.developers drop constraint developers_developer_company_id_fkey;
-alter table if exists public.developers drop constraint developers_developer_project_id_fkey;
+alter table if exists developers_skills drop constraint developers_skills_developer_id_fkey;
+alter table if exists developers_skills drop constraint developers_skills_skill_id_fkey;
+alter table if exists projects drop constraint projects_project_customer_id_fkey;
+alter table if exists projects drop constraint projects_project_company_id_fkey;
+alter table if exists developers drop constraint developers_developer_company_id_fkey;
+alter table if exists developers drop constraint developers_developer_project_id_fkey;
 
 drop table if exists SKILLS;
+drop table if exists CUSTOMERS;
+drop table if exists COMPANIES;
+drop table if exists PROJECTS;
+drop table if exists DEVELOPERS;
+drop table if exists DEVELOPERS_SKILLS;
 
 create table SKILLS(
 skill_id serial PRIMARY KEY NOT NULL,
 skill_name character varying NOT NULL
 );
-drop table if exists CUSTOMERS;
+
 
 create table CUSTOMERS(
 customer_id serial PRIMARY KEY NOT NULL,
 customer_name character varying NOT NULL
 );
 
-drop table if exists COMPANIES;
+
 
 create table COMPANIES(
 companie_id serial PRIMARY KEY NOT NULL,
 companie_name character varying NOT NULL
 );
 
-drop table if exists PROJECTS;
+
 
 create table PROJECTS(
 project_id serial  PRIMARY KEY NOT NULL,
@@ -39,7 +44,7 @@ project_customer_id integer references CUSTOMERS(customer_id) NOT NULL,
 project_start_timestamp date NOT NULL
 );
 
-drop table if exists DEVELOPERS;
+
 
 
 create table DEVELOPERS(
@@ -50,7 +55,7 @@ developer_project_id integer references PROJECTS (project_id) NOT NULL,
 developer_join_date date
 );
 
-drop table if exists DEVELOPERS_SKILLS;
+
 
 create table DEVELOPERS_SKILLS(
   developer_id integer references DEVELOPERS(developer_id) NOT NULL,
